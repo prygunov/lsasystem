906c8f408dc994ea3a66ebb77aa3b1b8b9d3a14f
Maxim <maksim.prygunov@gmail.com>
2020-11-13 21:28:27 +0400

    added StackException throw in LSAUtil

--- src/net/artux/utils/LSAUtil.java
+++ src/net/artux/utils/LSAUtil.java
@@ -1,5 +1,6 @@
 package net.artux.utils;
 
+import net.artux.exceptions.StackException;
 import net.artux.models.LSA;
 
 public class LSAUtil {
@@ -9,32 +10,48 @@ public class LSAUtil {
     public static void compileLSA(LSA lsa, Reader reader) {
         String v = lsa.getValue();
         commands = v.split(" ");
+        stackCount = 0;
         goTo('n', reader);
     }
 
+    static int stackCount;
+    static char lastIndex;
+
     private static void goTo(char index, Reader reader) {
         int i = 0;
-        if (index != 'n')
+        if (index != 'n') {
+            checkStack(index);
             for (int j = 0; j < commands.length; j++)
                 if (commands[j].contains("<"))
                     if (index == commands[j].charAt(0))
                         i = j;
 
-            for (; i < commands.length; i++)
-                if (commands[i].toLowerCase().contains("x")) {
-                    if (reader.next() == 0)
-                        if (commands[i + 1].contains(">")) {
-                            goTo(commands[i + 1].charAt(1), reader);
-                            break;
-                        }
-                } else if (commands[i].toLowerCase().contains("w")) {
+        }
+        for (; i < commands.length; i++)
+            if (commands[i].toLowerCase().contains("x")) {
+                if (reader.next() == 0)
                     if (commands[i + 1].contains(">")) {
                         goTo(commands[i + 1].charAt(1), reader);
                         break;
                     }
-                } else if(!commands[i].contains(">") && !commands[i].contains("<"))
-                    System.out.print(commands[i]);
+            } else if (commands[i].toLowerCase().contains("w")) {
+                if (commands[i + 1].contains(">")) {
+                    goTo(commands[i + 1].charAt(1), reader);
+                    break;
+                }
+            } else if(!commands[i].contains(">") && !commands[i].contains("<"))
+                System.out.print(commands[i]);
+    }
 
+    private static void checkStack(char index){
+        if (lastIndex == index)
+            stackCount++;
+        else {
+            lastIndex = index;
+            stackCount = 0;
+        }
+        if (stackCount>10)
+            throw new StackException();
     }
 
 }
