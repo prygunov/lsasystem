52a124d635aa40800afce399e4585f540597fc9d
Gribni4ok <mr.vlads1996@mail.ru>
2020-11-13 21:52:32 +0400

    Menu

--- src/net/artux/Main.java
+++ src/net/artux/Main.java
@@ -1,14 +1,48 @@
 package net.artux;
 
+import java.io.*;
 import net.artux.models.LSA;
 import net.artux.utils.LSAUtil;
 import net.artux.utils.Reader;
+
 
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+
+
+    public void Menu()
+    {
+        Scanner scanner = new Scanner(System.in);;
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("---------------МЕНЮ---------------");
+            System.out.println("1: Ввести ЛСА с клавиатуры");
+            System.out.println("2: Считать ЛСА из текстового файла");
+            System.out.println("3: Вывести ЛСА");
+            System.out.println("4: Выход");
+            switch (scanner.nextInt()) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Неверное значение");
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args)throws Exception{
         Reader reader = new Reader() {
             @Override
             public byte next() {
